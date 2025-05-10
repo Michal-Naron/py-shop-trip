e548d21885c8ac6f55bd37910a79be40ff25f0c6
Michal-Naron <michanaron@gmail.com>
2025-05-10 16:26:05 +0200

    Solution

--- app/car.py
+++ app/car.py
@@ -4,14 +4,12 @@ from typing import List
 
 class Car:
     def __init__(self, brand: str, fuel_consumption: float) -> None:
-        self.brand = brand
-        self.fuel_consumption = fuel_consumption
+        self.brand: str = brand
+        self.fuel_consumption: float = fuel_consumption
 
-    def cost_of_fuel_all_way(
-        self, customer_location: List[float], shop_location: List[float]
-    ) -> float:
-        distance = math.sqrt(
-            (customer_location[0] - shop_location[0]) ** 2
-            + (customer_location[1] - shop_location[1]) ** 2
+    def cost_of_fuel_all_way(self, customer_location: List[float], shop_location: List[float]) -> float:
+        distance: float = math.sqrt(
+            (customer_location[0] - shop_location[0]) ** 2 +
+            (customer_location[1] - shop_location[1]) ** 2
         )
         return (distance / 100) * self.fuel_consumption * 2
--- app/shop.py
+++ app/shop.py
@@ -2,19 +2,14 @@ from typing import Dict, List
 
 
 class Shop:
-    def __init__(
-        self,
-        name: str,
-        location: List[float],
-        products: Dict[str, float]
-    ) -> None:
-        self.name = name
-        self.location = location
-        self.products = products
+    def __init__(self, name: str, location: List[float], products: Dict[str, float]) -> None:
+        self.name: str = name
+        self.location: List[float] = location
+        self.products: Dict[str, float] = products
 
     def purchase(self, product_cart: Dict[str, int]) -> float:
-        price = 0.0
-        for product, quantity in product_cart.items():
-            if product in self.products:
-                price += quantity * self.products[product]
+        price: float = 0.0
+        for key, quantity in product_cart.items():
+            if key in self.products:
+                price += quantity * self.products[key]
         return round(price, 2)
